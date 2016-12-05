fbbbf98943dbe6a2eb4732c1e2a993a2d794b8b5
Alan McLeod <alan.mcleod@viability.io>
2016-12-05 14:08:30 +1030

    Kounta Integration - v4

--- order.go
+++ order.go
@@ -6,5 +6,19 @@ type Order struct {
 	SaleDate string  `json:"created_at"`
 	Status   string  `json:"status"`
 	Total    float64 `json:"total"`
-	TotalTax float64 `json:"total_tax"`
+
+	Items []OrderLine `json:"lines"`
+}
+
+type OrderLine struct {
+	TotalTax float64 `json:"line_total_tax"`
+}
+
+// GetTotalTax will return the total tax for an order
+func (order *Order) GetTotalTax() float64 {
+	t := 0.00
+	for _, item := range order.Items {
+		t += item.TotalTax
+	}
+	return t
 }
