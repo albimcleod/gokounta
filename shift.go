b11ecc487be3ffd8e745e9e1aeb62d1b30266863
albimcleod <albimcleod@gmail.com>
2017-09-23 14:19:41 +0930

    Support Shifts

---
+++ shift.go
@@ -0,0 +1,40 @@
+package gokounta
+
+import (
+	"net/http"
+
+	"github.com/mholt/binding"
+)
+
+//Shift is the struct for a Kounta Shift
+type Shift struct {
+	ID         int          `json:"id"`
+	StartedAt  string       `json:"started_at"`
+	FinishedAt string       `json:"finished_at"`
+	Staff      Staff        `json:"staff_member"`
+	Breaks     []ShiftBreak `json:"breaks"`
+}
+
+type ShiftBreak struct {
+	StartedAt  string `json:"started_at"`
+	FinishedAt string `json:"finished_at"`
+}
+
+//FieldMap is required for binding
+func (obj *Shift) FieldMap(req *http.Request) binding.FieldMap {
+	return binding.FieldMap{
+		&obj.ID:         "id",
+		&obj.StartedAt:  "started_at",
+		&obj.FinishedAt: "finished_at",
+		&obj.Staff:      "staff_member",
+		&obj.Breaks:     "breaks",
+	}
+}
+
+//FieldMap is required for binding
+func (obj *ShiftBreak) FieldMap(req *http.Request) binding.FieldMap {
+	return binding.FieldMap{
+		&obj.StartedAt:  "started_at",
+		&obj.FinishedAt: "finished_at",
+	}
+}
